a7674841f8b8ccbbc832fcc016d90088128e6cdf
Lucas Strand <lucas.strand@outlook.com>
2025-02-16 11:39:50 +0100

    Create westerm.lua

---
+++ westerm.lua
@@ -0,0 +1,163 @@
+local wezterm = require("wezterm")
+-- local mux = wezterm.mux 
+-- Pull in the wezterm API
+local act = wezterm.action
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.front_end = "OpenGL"
+config.max_fps = 144
+config.default_cursor_style = "BlinkingBlock"
+config.animation_fps = 1
+config.cursor_blink_rate = 500
+config.term = "xterm-256color" -- Set the terminal type
+
+config.cell_width = 0.9
+config.window_background_opacity = 0.9
+config.prefer_egl = true
+config.font_size = 18.0
+
+config.window_padding = {
+	left = 8,
+	right = 0,
+	top = 8,
+	bottom = 0,
+}
+
+-- tabs
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+-- This is where you actually apply your config choices
+--
+
+-- color scheme toggling
+wezterm.on("toggle-colorscheme", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if overrides.color_scheme == "Zenburn" then
+        overrides.color_scheme = "Cloud (terminal.sexy)"
+    else
+        overrides.color_scheme = "Zenburn"
+    end
+    window:set_config_overrides(overrides)
+end)
+
+-- keymaps
+config.keys = {
+	{
+		key = "E",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.EmitEvent("toggle-colorscheme"),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "U",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "O",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "P",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{ key = "9", mods = "CTRL", action = act.PaneSelect },
+	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
+	{
+		key = "O",
+		mods = "CTRL|SHIFT|ALT",
+		-- toggling opacity
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+			-- Get current opacity, falling back to the config value if not in overrides
+			local current_opacity = overrides.window_background_opacity or config.window_background_opacity
+			
+			-- Toggle between values
+			if current_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
+}
+
+-- For example, changing the color scheme:
+config.color_scheme = "Cloud (terminal.sexy)"
+config.colors = {
+	background = "#0c0b0f",
+	cursor_border = "#bea3c7",
+	cursor_bg = "#bea3c7",
+	-- selection_fg = '#feba17',
+
+	tab_bar = {
+		background = "#0c0b0f",
+		-- background = "rgba(0, 0, 0, 0%)",
+		active_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#bea3c7",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+		inactive_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#f8f2f5",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+
+		new_tab = {
+			-- bg_color = "rgba(59, 34, 76, 50%)",
+			bg_color = "#0c0b0f",
+			fg_color = "white",
+		},
+	},
+}
+
+config.window_frame = {
+	font = wezterm.font({ family = "Iosevka Custom", weight = "Regular" }),
+	active_titlebar_bg = "#0c0b0f",
+	border_left_width = "1px",
+	border_right_width = "1px",
+	border_top_height = "1px",
+	border_bottom_height = "1px",
+	border_left_color = "#bea3c7",
+	border_right_color = "#bea3c7",
+	border_bottom_color = "#bea3c7",
+	border_top_color = "#bea3c7",
+}
+
+
+config.window_decorations = "NONE | RESIZE"
+config.default_prog = { "powershell.exe", "-NoLogo" }
+config.initial_cols = 80
+
+-- and finally, return the configuration to wezterm
+return config
